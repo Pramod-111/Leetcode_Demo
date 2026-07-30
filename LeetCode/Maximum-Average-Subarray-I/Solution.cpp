9e5e956f80ff7f10edcba50fc5fa2d74b57f6874
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-07-30 09:15:42 +0530

    Add solution for Maximum Average Subarray I

--- LeetCode/Maximum-Average-Subarray-I/Solution.cpp
+++ LeetCode/Maximum-Average-Subarray-I/Solution.cpp
@@ -5,17 +5,17 @@
 5        int sum = 0;
 6
 7        for(int i = 0; i < k; i++) {
-8            sum += nums[i];
+8            sum += nums[i]; 
 9        }
-10        double maxAvg = (double)sum / k;
-11
-12        for(int right = k; right < n; right++) {
-13            sum += nums[right];
-14            sum -= nums[right - k];
-15            double avg = (double)sum / k;
-16
-17            maxAvg = max(avg, maxAvg);
-18        } 
+10        
+11        double maxAvg = (double)sum / k;
+12
+13        for(int i = k; i < n; i++) {
+14            sum += nums[i];
+15            sum -= nums[i - k];
+16            double avg = (double)sum / k;
+17            maxAvg = max(maxAvg, avg);
+18        }
 19        return maxAvg;
 20    }
 21};
